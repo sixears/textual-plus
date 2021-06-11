51f4542113847628fc80655cfe80ea0431a50284
Mr. Fluffy <git@sixearsm>
2021-06-11 17:07:44 +0000

    r1.0.1.0

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+1.0.1.0 2021-06-11
+==================
+- incorporate Fluffy.Printable
+
 1.0.0.0 2019-01-02
 ==================
 - factored out from minfo
--- src/TextualPlus.hs
+++ src/TextualPlus.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-{-# LANGUAGE RankNTypes        #-}
-{-# LANGUAGE UnicodeSyntax     #-}
-{-# LANGUAGE ViewPatterns      #-}
-
 module TextualPlus
-  ( PrintOut( toP ), parseTextual )
+  ( PrintOut( toP ), parseTextual
+  , __ERR__, __error__
+  , bracket, bracket', b, b'
+  , encompass, encompass'
+  , guillemet, guillemet', g, g'
+  , parenthesize, parenthesize', p, p'
+  , quote, quote', q, q'
+  , qquote, qquote', qq, qq'
+  , surround, surround'
+  )
 where
 
 import Prelude  ( error )
@@ -21,6 +23,7 @@ import Data.Typeable  ( Typeable, typeOf )
 -- base-unicode-symbols ----------------
 
 import Data.Function.Unicode  ( (∘) )
+import Data.Monoid.Unicode    ( (⊕) )
 
 -- data-textual ------------------------
 
@@ -71,5 +74,97 @@ parseTextual (toText → z) =
         Malformed xs x → let msg = [fmtT|failed to parse '%t' as '%w': [%L] %s|]
                                    z typ xs x
                           in throwError (toP msg)
+
+{- | throw an error with some printable -}
+__ERR__ ∷ Printable ρ ⇒ ρ → α
+__ERR__ s = error $ toString s
+
+{- | throw an error -}
+__error__ ∷ Text → α
+__error__ = __ERR__
+
+{- | adorn text with a prefix and a suffix -}
+encompass ∷ Printable ρ ⇒ Text → Text → ρ → Text
+encompass pfx sfx t = pfx ⊕ toText t ⊕ sfx
+
+encompass' ∷ Text → Text → Text → Text
+encompass' = encompass
+
+{- | adorn text with (the same) prefix suffix -}
+surround ∷ Printable ρ ⇒ Text → ρ → Text
+surround x = encompass x x
+
+surround' ∷ Text → Text → Text
+surround' = surround
+
+{- | surround text with (single) quotes -}
+quote ∷ Printable ρ ⇒ ρ -> Text
+quote = surround "'"
+
+quote' ∷ Text -> Text
+quote' = quote
+
+{- | short alias for `quote` -}
+q ∷ Printable ρ ⇒ ρ -> Text
+q = quote
+
+q' ∷ Text -> Text
+q' = quote'
+
+{- | surround text with (double) quotes -}
+qquote ∷ Printable ρ ⇒ ρ -> Text
+qquote = surround "\""
+
+qquote' ∷ Text -> Text
+qquote' = qquote
+
+{- | short alias for `qquote` -}
+qq ∷ Printable ρ ⇒ ρ -> Text
+qq = qquote
+
+qq' ∷ Text -> Text
+qq' = qquote'
+
+{- | surround text with parentheses -}
+parenthesize ∷ Printable ρ ⇒ ρ → Text
+parenthesize = encompass "(" ")"
+
+parenthesize' ∷ Text → Text
+parenthesize' = parenthesize
+
+{- | short alias for `parenthesize` -}
+p ∷ Printable ρ ⇒ ρ -> Text
+p = parenthesize
+
+p' ∷ Text -> Text
+p' = parenthesize'
+
+{- | surround text with brackets -}
+bracket ∷ Printable ρ ⇒ ρ → Text
+bracket = encompass "[" "]"
+
+bracket' ∷ Text → Text
+bracket' = bracket
+
+{- | short alias for `bracket` -}
+b ∷ Printable ρ ⇒ ρ -> Text
+b = bracket
+
+b' ∷ Text -> Text
+b' = bracket'
+
+{- | surround text with guillemets -}
+guillemet ∷ Printable ρ ⇒ ρ → Text
+guillemet = encompass "«" "»"
+
+guillemet' ∷ Text → Text
+guillemet' = guillemet
+
+{- | short alias for `guillemet` -}
+g ∷ Printable ρ ⇒ ρ -> Text
+g = guillemet
+
+g' ∷ Text -> Text
+g' = guillemet'
 
 -- that's all, folks! ----------------------------------------------------------
