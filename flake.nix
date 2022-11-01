5ff7c266ffbfe9c231aa96af7c1f68a226946335
Mr. Fluffy <git@sixearsm>
2022-11-01 16:00:47 +0000

    upgrade flake-build-utils

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "manage info.yaml";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.3";
+
+    tfmt.url         = "github:sixears/tfmt/r0.2.7.1";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, build-utils
+            , tfmt }:
+    build-utils.lib.hOutputs self nixpkgs "textual-plus" {
+      deps = {
+        inherit tfmt;
+      };
+      ghc = p: p.ghc8107; # for tfmt
+    };
+}
