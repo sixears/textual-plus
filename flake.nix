a865e488480135ac748ff679bac1e63937c0a4f3
Mr. Fluffy <git@sixearsm>
2022-11-19 06:39:22 +0000

    natural->0.0.1.13

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    tfmt.url         = github:sixears/tfmt/r0.2.7.21;
+    tfmt.url         = github:sixears/tfmt/r0.2.7.22;
   };
 
   outputs = { self, nixpkgs, build-utils
